5a22863d61073563feaa5ca8c495ccb531134e5b
Fortune Uche <fortuneeziefula3@gmail.com>
2021-11-22 17:29:51 +0100

    I commmited this today

---
+++ main.js
@@ -0,0 +1,52 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoBtn = document.querySelector('.add-btn');
+const todoList = document.querySelector('.todo-list');
+
+//Event listeners
+
+todoBtn.addEventListener("click",addTodo);
+todoList.addEventListener("click",deleteCheck);
+
+//Functions 
+
+function addTodo(event){
+    event.preventDefault();
+    
+
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add('todo');
+    
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class = "fa fa-check"></i> ';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class = "fa fa-trash"></i> ';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+todoInput.value = "";
+}
+
+
+function deleteCheck(e){
+    const item = e.target;
+     
+    if (item.classList[0] === 'trash-btn'){
+       const todo = item.parentElement;
+        todo.remove();
+    }
+  
+    if (item.classList[0] === 'complete-btn'){
+       const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
